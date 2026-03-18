c5ad195a6e39b28ba13bce1ad4c3a4acef997a9b
janvi-sarvaiya <janvisarvaiya08.bs@gmail.com>
2026-03-18 14:15:41 +0530

    create contact page

--- Frontend/my-app/src/pages/ProductDetail.jsx
+++ Frontend/my-app/src/pages/ProductDetail.jsx
@@ -6,7 +6,7 @@ import { useDispatch } from "react-redux";
 import Navbar from "../components/layout/Navbar";
 import Breadcrumbs from "../components/common/Breadcrumbs";
 import Rating from "@mui/material/Rating";
-import QunatityBox from "../components/product/QunatityBox";
+import QuantityBox from "../components/product/QuantityBox";
 import Footer from "../components/layout/Footer";
 import { useUser } from "@clerk/clerk-react";
 
@@ -15,6 +15,7 @@ import { TbTruckDelivery } from "react-icons/tb";
 import { LuRefreshCcw } from "react-icons/lu";
 import ProductImageGallery from "../components/product/ProductImageGallery";
 import { toast } from "react-toastify";
+
 
 export default function ProductDetail() {
   const dispatch = useDispatch();
@@ -94,7 +95,7 @@ export default function ProductDetail() {
             )}
 
             <div className="mt-5 flex items-center gap-4">
-              <QunatityBox quantity={quantity} setQunatity={setQunatity} />
+              <QuantityBox quantity={quantity} setQunatity={setQunatity} />
               <button
                 onClick={handleAddToCart}
                 className={`text-white py-2.5 px-12 rounded ${product.inStock ? "bg-orange cursor-pointer" : "bg-[#ca3c3c] cursor-not-allowed"}`}
